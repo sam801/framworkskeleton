72fec1e5be3e374b86c9b8642291021cb36d5dc8
meashoss <meashoss@gmail.com>
2012-05-27 02:09:36 +0000

    some persistence features of our framework is added to sample application.

--- Skeleton/src/main/scala/ourSkeleton/MyApplication.scala
+++ Skeleton/src/main/scala/ourSkeleton/MyApplication.scala
@@ -33,6 +33,7 @@ import org.scribe.model._
 import org.scribe.oauth._
 import ourSkeleton.routing._
 import ourSkeleton.Template._
+import ourSkeleton.Persistance._
 import scala.util.parsing.json.JSON
 class MyApplication extends HttpServlet {
 
@@ -40,23 +41,34 @@ class MyApplication extends HttpServlet {
   /**
    *  Creating an object of Routing and Tempate class for later uses.
    */
-  val routing =  new Routing()
+  val routing = new Routing()
   val template = new Template()
-
+  val persistance = new Persistence()
   override def init(config: ServletConfig) {
     super.init(config)
 
-    ////////////////////////////////////////////
-    // 					  					  //
-    //		YOU CAN ADD ROUT HERE			  //
-    // 										  //
-    ////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+    //    									DATABASE CONNECTION
+    //    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Class.forName("org.h2.Driver")
+    SessionFactory.concreteFactory = Some(() =>
+      Session.create(java.sql.DriverManager.getConnection("jdbc:h2:" + System.getProperty("user.dir") + "/test;AUTO_SERVER=TRUE", "sa", "sa"), new H2Adapter))
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+    //    									HERE YOU CAN ADD YOU ROUTE
+    //    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     routing.addRoute("/", main)
     routing.addRoute("/documentation", documentation)
-    routing.addRoute("/code", code)
     routing.addRoute("/model", model)
     routing.addRoute("/download", download)
+    routing.addRoute("/blog", blog)
+    routing.addRoute("/newApp", creatNewApp)
+    routing.addRoute("/anatomy", anatomy)
+    routing.addRoute("/ide", ide)
+    routing.addRoute("/sampleApp", sampleApp)
     routing.addRoute("/mainCss.css", css)
   }
 
@@ -81,8 +93,8 @@ class MyApplication extends HttpServlet {
     resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
   }
 
-  def code(req: HttpServletRequest, resp: HttpServletResponse) = {
-    val xml = template.setUpTemplate("code.xml")
+  def blog(req: HttpServletRequest, resp: HttpServletResponse) = {
+    val xml = template.setUpTemplate("blog.xml")
     val html = prepareForm(xml)
     resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
   }
@@ -95,6 +107,12 @@ class MyApplication extends HttpServlet {
 
   def download(req: HttpServletRequest, resp: HttpServletResponse) = {
     val xml = template.setUpTemplate("download.xml")
+    val html = prepareForm(xml)
+    resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
+  }
+  
+  def sampleApp(req: HttpServletRequest, resp: HttpServletResponse) = {
+    val xml = template.setUpTemplate("sampleApp.xml")
     val html = prepareForm(xml)
     resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
   }
@@ -111,6 +129,163 @@ class MyApplication extends HttpServlet {
     resp.getWriter.write(js)
     resp.setContentType("text/css")
 
+  }
+  
+  def creatNewApp(req: HttpServletRequest, resp: HttpServletResponse) = {
+    val xml = template.setUpTemplate("newApp.xml")
+    val html = prepareForm(xml)
+    resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
+  }
+  
+  def anatomy(req: HttpServletRequest, resp: HttpServletResponse) = {
+    val xml = template.setUpTemplate("anatomy.xml")
+    val html = prepareForm(xml)
+    resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
+  }
+  
+  def ide(req: HttpServletRequest, resp: HttpServletResponse) = {
+    val xml = template.setUpTemplate("ide.xml")
+    val html = prepareForm(xml)
+    resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
+  }
+  
+  /*
+  * register 
+  */
+
+  def Register(name: String, last: String, email: String, pass: String) = {
+
+    var toReturn: Option[Long] = None
+
+    transaction {
+
+      var res = from(Forum.forum_users)(c =>
+        where(c.email === email)
+          select (c.id))
+
+      if (res != null)
+        toReturn = res.headOption
+
+    }
+
+    toReturn match {
+      case Some(id: Long) =>
+
+        repeatation
+      case _ =>
+        if (name.isEmpty() || last.isEmpty() || email.isEmpty() || pass.isEmpty()) {
+
+        } else {
+
+          LogOut(name, last, email, pass)
+        }
+
+    }
+  }
+
+  //handler function for SignOut
+  def LogOut(name: String, last: String, email: String, pass: String) = {
+
+    transaction {
+      Forum.forum_users.insert(new User(name, last, email, pass))
+    }
+
+    <html>
+      <head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="mainCss.css"/>
+      </head>
+      <body>
+        <header>
+          <h1 id="welcome">Welcome To Our Framework</h1>
+        </header>
+        <nav>
+          <ul>
+            <li class="active" id="Documentation"><a href="http://localhost:8080/documentation">Documentation</a></li>
+            <li class="active" id="Model"><a href="http://localhost:8080/model">Model</a></li>
+            <li class="active" id="Blog"><a href="http://localhost:8080/blog">Blog</a></li>
+            <li class="active" id="Download"><a href="http://localhost:8080/download">Download</a></li>
+          </ul>
+        </nav>
+        <section>
+          <article>
+            <h1>Congratulations { name }</h1>
+            <h2>your registration is complete!</h2>
+          </article>
+          <aside>
+            <img id="myImage" src="http://www.lss.lu.unisi.ch/pictures/usi_partialview.jpg" alt="myImage"/>
+          </aside>
+        </section>
+        <footer>
+          <p>2011 fatima and samuel, Last Update!!</p>
+        </footer>
+      </body>
+    </html>
+  }
+
+  def repeatation = {
+
+    <html>
+      <head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="mainCss.css"/>
+      </head>
+      <body>
+        <header>
+          <h1 id="welcome">Welcome To Our Framework</h1>
+        </header>
+        <nav>
+          <ul>
+            <li class="active" id="Documentation"><a href="http://localhost:8080/documentation">Documentation</a></li>
+            <li class="active" id="Model"><a href="http://localhost:8080/model">Model</a></li>
+            <li class="active" id="Blog"><a href="http://localhost:8080/blog">Blog</a></li>
+            <li class="active" id="Download"><a href="http://localhost:8080/download">Download</a></li>
+          </ul>
+        </nav>
+        <section>
+          <article>
+            <h2>  This email is already reagistered!!</h2>
+          </article>
+          <aside>
+            <img id="myImage" src="http://www.lss.lu.unisi.ch/pictures/usi_partialview.jpg" alt="myImage"/>
+          </aside>
+        </section>
+        <footer>
+          <p>2011 fatima and samuel, Last Update!!</p>
+        </footer>
+      </body>
+    </html>
+  }
+
+  def getAllUsers(req: HttpServletRequest, resp: HttpServletResponse) {
+
+    val xml = ListUsers
+    val html = prepareForm(xml)
+    resp.getWriter.write(new PrettyPrinter(72, 2).formatNodes(html))
+  }
+
+  //show all accounts
+  def ListUsers = {
+
+    <html>
+      <head>
+        <title>Accounts:</title>
+      </head>
+      <body>
+        <h3>All accounts</h3>
+        <table border="1">
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>password</th>
+          </tr>
+          {
+            persistance.getUsers
+
+          }
+        </table>
+      </body>
+    </html>
   }
 
   /**
